2605bbe80bb2b3c2ba4e9e82bb0a82bb63f2c872
Aliya Zahira Nadra <158556910+lollyyy@users.noreply.github.com>
2024-09-08 23:43:03 +0700

    menambahkan url deployment ke settings.py

--- sole_de_luxe/settings.py
+++ sole_de_luxe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l7900fr9ipu%)_)qvv1z62$!u5w&$x6_!$ls#&qw11+y_jg1&g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "aliya-zahira-soledeluxe.pbp.cs.ui.ac.id"]
 
 
 # Application definition
